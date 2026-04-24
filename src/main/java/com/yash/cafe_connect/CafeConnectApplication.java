b56dc8117af342d7c6ea3f7e767633661c4f55c0
Pavan-Suryavanshi <pavansuryavanshi52@gmail.com>
2026-04-24 12:05:06 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/yash/cafe_connect/CafeConnectApplication.java
@@ -0,0 +1,13 @@
+package com.yash.cafe_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/cafe_connect/CafeConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.cafe_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
